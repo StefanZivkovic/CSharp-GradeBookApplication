53ea5fb1f86078c0c0e1ccae8dfadeb34979ecfe
StefanZivkovic <zivkovicstefan123@gmail.com>
2019-10-22 15:21:45 +0200

    RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,30 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;  
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (base.Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade > 80)
+                return 'A';
+            else if (averageGrade > 60)
+                return 'B';
+            else if (averageGrade > 40)
+                return 'C';
+            else if (averageGrade > 20)
+                return 'D';
+            return 'F';
+            //return base.GetLetterGrade(averageGrade);
         }
     }
 }
